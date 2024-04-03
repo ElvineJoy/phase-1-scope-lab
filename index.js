f49566bf13224f58a5c545efd3da64661dcd35ea
ElvineJoy <elvinkuiyungest@gmail.com>
2024-04-03 17:21:51 +0300

    Scope lab- most difficult for me

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+function upperCaseCustomerName () {
+   var nameCustomer = customerName.toUpperCase()
+   customerName = nameCustomer
+   console.log(customerName);
+}    
+// var customerName = upperCaseCustomerName('bob')
+// console.log(customerName);
+
+// third instruction
+function setBestCustomer () {
+    bestCustomer = "not bob";
+}
+
+// fourth
+function overwriteBestCustomer () {
+    bestCustomer = "maybe bob"
+}
+
+// last
+const leastFavoriteCustomer = 'Me';
+
+function changeLeastFavoriteCustomer () {
+    leastFavoriteCustomer = 'Lin'
+}
+console.log(changeLeastFavoriteCustomer())
